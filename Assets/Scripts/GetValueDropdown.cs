8c213de608b4df52c80235a7c8c3910c12543498
jeromar <jmartinez.carabali@udea.edu.co>
2025-11-02 01:47:03 -0500

    se me olvido guardar el archivo
    funciona de cualquier modo pero no se deberia de usar ningun parametro de este script que no este en la version actualizada

--- Assets/Scripts/GetValueDropdown.cs
+++ Assets/Scripts/GetValueDropdown.cs
@@ -5,13 +5,10 @@ using System.Collections.Generic;
 public class GetValueDropdown : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown dropdown;
-    const int principiante = 0; // indice del modo personalizado
-    const int intermedio = 1; // indice del modo personalizado
-    const int experto = 2; // indice del modo personalizado
-    const int personalizado = 3; // indice del modo personalizado
+    
 
     // atributos publicos a otros scripts 
-    public static int[,] matrizGenerada;
+    
     public int modoSeleccionado;
     public void GetDropdownValue()
 
@@ -21,30 +18,7 @@ public class GetValueDropdown : MonoBehaviour
         modoSeleccionado = indice;
         Debug.Log(opcion+":"+indice); // se muestra en la consola para debug 
         
-        int filas = 0, columnas = 0, minas = 0;
-
-        // se genera la matriz correspondiente 
-        switch (indice)
-        {
-            case principiante:
-                filas = 8; columnas = 8; minas = 10;
-                break;
-            case intermedio:
-                filas = 16; columnas = 16; minas = 40;
-                break;
-            case experto:
-                filas = 16; columnas = 30; minas = 99;
-                break;
-            case personalizado:
-                filas = 10; columnas = 10; minas = 20; // puedes pedir valores personalizados
-                break;
-            default:
-                Debug.LogWarning("Modo no reconocido");  // TOdo: manejar error
-                break;
-        }
-        matrizGenerada = GeneradorMinas.GenerarMinas(filas, columnas, minas);
-
-        Debug.Log(matrizGenerada);
+       
     }
 
     // Update is called once per frame
