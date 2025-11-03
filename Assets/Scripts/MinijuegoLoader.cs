66824ff4c46b586f8d2ac40c57e2cd0e2e31aca2
jhoandaza <jhoan.daza@udea.edu.co>
2025-11-02 22:18:38 -0500

    Cargador de juegos random
    xdxd

--- Assets/Scripts/MinijuegoLoader.cs
+++ Assets/Scripts/MinijuegoLoader.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class MinijuegoLoader : MonoBehaviour
 {
-   public GameObject miniGamePanel;
-
-    void Start()
+    public GameObject[] minijuegos;
+    
+       void Start()
     {
-        if (miniGamePanel != null)
+        if (minijuegos.Length == 0)
         {
-            miniGamePanel.SetActive(true);
+            Debug.LogWarning("No hay minijuegos asignados en el inspector.");
+            return;
         }
-        else
-        {
-            Debug.LogWarning("MiniGamePanel no asignado en el inspector.");
-        }
+
+        // Desactivar todos los minijuegos
+        foreach (var m in minijuegos)
+            m.SetActive(false);
+
+        // Elegir uno al azar
+        int index = Random.Range(0, minijuegos.Length);
+        minijuegos[index].SetActive(true);
     }
+    
 }
